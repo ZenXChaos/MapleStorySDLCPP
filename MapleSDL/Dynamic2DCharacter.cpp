6d3adac1cfc40b1fc51210b4c71485039b74a87e
ZenXChaos <psilocybin2k@gmail.com>
2016-04-30 19:47:25 -0600

    POSIX paths

--- MapleSDL/Dynamic2DCharacter.cpp
+++ MapleSDL/Dynamic2DCharacter.cpp
@@ -52,16 +52,19 @@ Dynamic2DCharacter::Dynamic2DCharacter(Player* mainPlayer) : Player(*mainPlayer)
 {
 	Dynamic2DBodyPart head;
 	head.bp_Animations["idle"] = *(new AnimatedSprite());
+	head.bp_Animations["idle"].LoadTexture("content/Dynamic2D/01/idle.head.png", m_gRenderer);
 	head.bp_Animations["idle"].BuildAnimation(0, 3, 123 / 3, 37, 0.1f);
 
 	Dynamic2DBodyPart body;
 	body.bp_Animations["idle"] = *(new AnimatedSprite());
+	body.bp_Animations["idle"].LoadTexture("content/Dynamic2D/01/idle.body.png", m_gRenderer);
 	body.bp_Animations["idle"].BuildAnimation(0, 3, 66 / 3, 31, 0.1f);
 	body.relX = 7;
 	body.relY = 35;
 
 	Dynamic2DBodyPart arms;
 	arms.bp_Animations["idle"] = *(new AnimatedSprite());
+	arms.bp_Animations["idle"].LoadTexture("content/Dynamic2D/01/idle.arm.png", m_gRenderer);
 	arms.bp_Animations["idle"].BuildAnimation(0, 3, 31 / 3, 19, 0.1f);
 	arms.relX = 23;
 	arms.relY = 37;
